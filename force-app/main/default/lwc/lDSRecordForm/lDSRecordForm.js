90fdf5a63b28d880ef7308b9fd19ab467eba10ba
senolyildiz <senolyildiz9801@gmail.com>
2024-09-18 06:06:46 +0100

    wed

--- force-app/main/default/lwc/lDSRecordForm/lDSRecordForm.js
+++ force-app/main/default/lwc/lDSRecordForm/lDSRecordForm.js
@@ -1,3 +1,19 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track} from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+export default class LDSRecordForm extends LightningElement {
+    fields = ['Name', 'Phone', 'Industry', 'Type'];
+    @api recordId;
 
-export default class LDSRecordForm extends LightningElement {}
+    handleSuccess(event){
+        const evt = new ShowToastEvent({
+            title: "Woov",
+            message: "An account was successfully created",
+            variant: "success"
+        });
+        
+       
+        this.dispatchEvent(evt);
+    }
+
+
+}
