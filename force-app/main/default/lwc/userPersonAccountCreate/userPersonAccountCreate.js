67805d8884aa5a60ab5dd50d855a6108f229b231
senolyildiz <senolyildiz9801@gmail.com>
2024-09-19 12:23:37 +0100

    zz

--- force-app/main/default/lwc/userPersonAccountCreate/userPersonAccountCreate.js
+++ force-app/main/default/lwc/userPersonAccountCreate/userPersonAccountCreate.js
@@ -1,3 +1,31 @@
 import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+export default class UserPersonAccountCreate extends LightningElement {
 
-export default class UserPersonAccountCreate extends LightningElement {}
+   
+
+    handleSuccess(event){
+        
+        const inputFields = this.template.querySelectorAll('lightning-input-field');
+        inputFields.forEach( field => {
+            field.reset();
+        });
+        const editForm = this.template.querySelector('lightning-record-form');
+        editForm.recordId = null;
+        
+        const evt = new ShowToastEvent({
+            title: "Woov",
+            message: "A Patient Account was successfully created",
+            variant: "success"
+        });
+        
+        this.dispatchEvent(evt);
+    }
+
+    handleToggle() {
+        this.showForm = !this.showForm;
+   
+
+
+
+}
